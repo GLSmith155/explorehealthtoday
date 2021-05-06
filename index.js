1e2553b4263fc49de57659162b3536cfaff7f46d
George Smith <40671362+GLSmith155@users.noreply.github.com>
2021-05-05 22:19:18 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Main from "./components/Main";
+import "./index.css";
+import 'semantic-ui-css/semantic.min.css';
+import "./components/index.css";
+ 
+ReactDOM.render(
+  <Main/>, 
+  document.getElementById("root")
+);
